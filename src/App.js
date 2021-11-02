78a58bd438cddf1bcb4f516cc29e43fdf79843c1
Your Name <tbengtson82@gmail.com>
2021-11-02 13:11:17 -0500

    Axios call and state built

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
+import axios from "axios"
+import {BASEURL, KEY_CONNECT, API_KEY} from "./constants/constants";
 
 function App() {
+    const [data, setData] = useState({})
+
+    useEffect(() =>{
+        axios.get(BASEURL + KEY_CONNECT + API_KEY)
+            .then(res => {
+                console.log(res.data)
+                setData(res.data)
+            })
+            .catch(err => {
+                console.error(err)
+            })
+
+    },[])
+
+
+
   return (
     <div className="App">
       <p>
---
+++ src/constants/constants.js
@@ -0,0 +1,12 @@
+export const BASEURL = `https://api.nasa.gov/planetary/apod`;
+export const API_KEY = `N0PKz6pozv0kkY5TL2E6JkJ6JEFF2FEynWpGkxga`;
+export const KEY_CONNECT = `?api_key=`;
+
+
+
+
+
+
+
+
+
