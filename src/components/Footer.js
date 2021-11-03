a72241856992f6906f632598ab8b4a66d3e53f90
Your Name <tbengtson82@gmail.com>
2021-11-03 17:35:58 -0500

    more styling

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,24 +3,36 @@ import styled from "styled-components";
 
 const Styled_div = styled.div`
   background-color: darkslategrey;
-    color: lightgray;
+  color: lightgray;
+  padding: 2%;
+  display: flex;
+  justify-content: space-around;
+  transition: color 2s;
+  
+  transition-timing-function: ease-in;
+
 `
+
+const Styled_a = styled.a`
+  text-decoration: none;
+  color: lightgray;
+  &:hover {
+    color: grey;
+    font-size: 1.15rem;
+    transition: font-size 1s;
+    
+  }
+  
+`
+
 
 export default function Footer(props) {
     const {data} = props;
 
     return(
         <Styled_div>
-            <p>CopyRight:&nbsp; {data.copyright}&nbsp;&#169;</p>
-            <p></p>
-            {/*<br/>*/}
-
-            {/*<br/>*/}
-            {/*<br/>*/}
-            {/*<br/>*/}
-            {/*<br/>*/}
-            {/*<br/>*/}
-
+            <p>{data.copyright}&nbsp;&#169;</p>
+            <Styled_a href={data.hdurl}>Link to HD Image</Styled_a>
         </Styled_div>
 
     )
