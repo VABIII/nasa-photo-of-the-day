b2aa31f264b8f85f47cd267b614fbba69f7174c6
Your Name <tbengtson82@gmail.com>
2021-11-03 15:50:10 -0500

    Styled search component

--- src/components/Search.js
+++ src/components/Search.js
@@ -7,18 +7,23 @@ const Styled_button = styled.button`
   border: none;
   font-weight: bold;
   font-size: 1.25rem;
-  border: 1px red solid;
+  
+  
+  &:hover {
+    font-size: 1.5rem;
+    border: 1px lightgray solid;
+    border-radius: 10%;
+  } 
 `
 
 const Styled_input = styled.input`
-  color: darkslategrey;
-  border: 1px solid red;
+  color: darkslategrey;  
 `
 const Styled_form = styled.form`
-  border: 1px solid red;
-  //display: flex;
+  padding: 15% 0 5%;
+  display: flex;
+  align-items: flex-end;
 `
-
 
 export default function Search(props) {
  const {onSubmit, onChange} = props;
@@ -26,8 +31,8 @@ export default function Search(props) {
     return (
         <div>
             <Styled_form>
+                <Styled_button formAction="submit" onClick={onSubmit}>Search:&nbsp;</Styled_button>
                 <Styled_input  type="date" onChange={onChange}  />
-                <Styled_button formAction="submit" onClick={onSubmit}>Search</Styled_button>
             </Styled_form>
         </div>
     )
